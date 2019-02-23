66a21431d7cc4c81f058d1ad902b29959f9961c2
PabloRR100 <pabloruizruiz10@gmail.com>
2019-02-23 16:50:37 +0100

    adjust lr

--- scripts/convnet.py
+++ scripts/convnet.py
@@ -24,7 +24,7 @@ from beautifultable import BeautifulTable as BT
 avoidWarnings()
 ## Note: the paper doesn't mention about trainining epochs/iterations
 parser = argparse.ArgumentParser(description='Recursive Networks with Ensemble Learning')
-parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
+parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
 parser.add_argument('--layers', '-L', default=16, type=int, help='# of layers')
 parser.add_argument('--batch', '-bs', default=128, type=int, help='batch size')
 parser.add_argument('--epochs', '-e', default=200, type=int, help='num epochs')
@@ -213,7 +213,7 @@ def results_backup(epoch):
         with open('Results_Singe.pkl', 'wb') as object_result:
                 pickle.dump(results, object_result, pickle.HIGHEST_PROTOCOL)     
 
-#@timeit
+@timeit
 def run_epoch(epoch):
     
     lr_schedule(epoch)
