c7f2eb118cbe8cc741482ecb3426403ef3aaf349
PabloRR100 <pabloruizruiz10@gmail.com>
2019-02-24 13:13:55 +0100

    editing recursive

--- scripts/models.py
+++ scripts/models.py
@@ -3,6 +3,125 @@ import math
 import torch
 from torch import nn
 
+    
+# Convolutional Networks
+        
+class Conv_Net(nn.Module):
+    
+    def __init__(self, name:str, layers:int, filters:int=32):
+        super(Conv_Net, self).__init__()
+        
+        self.name = name
+        self.L = layers
+        self.M = filters
+        self.act = nn.ReLU(inplace=True)
+    
+        self.V = nn.Conv2d(3,self.M,8, stride=1, padding=3)     
+        self.P = nn.MaxPool2d(4, stride=4, padding=2)           
+        
+        self.W = nn.ModuleList(                                 
+            [nn.Conv2d(32,32,3, padding=1) for _ in range(self.L)])   
+        
+        self.fc = nn.Linear(8*8*self.M, 10)
+        
+        # Custom Initialization
+        """for name, param in self.named_parameters():
+            
+            # Vm has zero mean and 0.1 std (0.01 var)
+            if 'V' in name and 'weight' in name:
+                param.data.normal_(0, 0.1)
+            
+            # W are initialized with the identity matrix - Kronecker delta
+            elif 'W' in name and 'weight' in name:
+                param.data.fill_(0)
+                for i in range(32):
+                    param.data[i][0][0][0].fill_(1)
+                    
+                        ## TODO: C is not specified in the paper
+            elif 'fc' in name and 'bias' in name:
+                param.data.fill_(0)"""
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+                        
+    def forward(self, x):
+        
+        x = self.act(self.V(x))         # Out: 32x32xM  -- Maybe padding = 4?
+        x = self.P(x)                   # Out: 8x8xM  -- Check also padding here
+        for w in self.W:                
+            x = self.act(w(x))          # Out: 8x8xM  -- Check also padding here)]
+        x = x.view(x.size(0), -1)       # Out: 64*M  (M = 32 -> 2048)
+        return self.fc(x)
+
+
+
+class Conv_Recusive_Net(nn.Module):
+    
+    def __init__(self, name:str, layers:int, filters:int=32):
+        super(Conv_Recusive_Net, self).__init__()
+        
+        self.name = name
+        self.L = layers
+        self.M = filters
+        self.act = nn.ReLU(inplace=True)
+
+        self.V = nn.Conv2d(3,self.M,8, stride=1, padding=3)     # Out: 32x32xM
+        self.P = nn.MaxPool2d(4, stride=4, padding=2)           # Out: 8x8xM  -- Check also padding here
+        
+        self.W = nn.Conv2d(self.M,self.M,3, padding=1)          # Out: 8x8xM  -- Check also padding here)]
+        
+        self.fc = nn.Linear(8*8*self.M, 10)
+        
+        # Custom Initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+                        
+        
+    def forward(self, x):
+        
+        x = self.act(self.V(x))
+        x = self.P(x)
+        for w in range(self.L):
+            x = self.act(self.w(x))
+        x = x.view(x.size(0), -1)
+        return self.fc(x)    
+    
+
+
+if '__name__' == '__main__':
+    
+    from torch.autograd import Variable
+    def test(net):
+        y = net(Variable(torch.randn(1,3,32,32)))
+        print(y.size())
+    
+    L = 16
+    M = 32
+    
+    convnet = Conv_Net('ConvNet', layers=L, filters=M)
+    r_convnet = Conv_Recusive_Net('RecursiveConvNet', layers=L, filters=M)
+    
+    test(convnet)
+    test(r_convnet)
+    
+    
+    
 # Fully Connected Networks
 
 class FC_Net(nn.Module):
@@ -48,110 +167,3 @@ class FC_Recursive_Net(nn.Module):
         for l in range(self.n_lay): 
             x = self.actf(self.fcHid(x))            
         return self.fcO(x)
-
-    
-# Convolutional Networks
-        
-class Conv_Net(nn.Module):
-    
-    def __init__(self, name:str, layers:int, filters:int=32):
-        super(Conv_Net, self).__init__()
-        
-        self.name = name
-        self.L = layers
-        self.M = filters
-        self.act = nn.ReLU(inplace=True)
-    
-        self.V = nn.Conv2d(3,self.M,8, stride=1, padding=3)     
-        self.P = nn.MaxPool2d(4, stride=4, padding=2)           
-        
-        self.W = nn.ModuleList(                                 
-            [nn.Conv2d(32,32,3, padding=1) for _ in range(self.L)])   
-        
-        self.fc = nn.Linear(8*8*self.M, 10)
-        
-        # Custom Initialization
-        """for name, param in self.named_parameters():
-            
-            # Vm has zero mean and 0.1 std (0.01 var)
-            if 'V' in name and 'weight' in name:
-                param.data.normal_(0, 0.1)
-            
-            # W are initialized with the identity matrix - Kronecker delta
-            elif 'W' in name and 'weight' in name:
-                param.data.fill_(0)
-                for i in range(32):
-                    param.data[i][0][0][0].fill_(1)"""
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
-                
-            ## TODO: C is not specified in the paper
-#            elif 'fc' in name and 'bias' in name:
-#                param.data.fill_(0)
-        
-    def forward(self, x):
-        
-        x = self.act(self.V(x))         # Out: 32x32xM  -- Maybe padding = 4?
-        x = self.P(x)                   # Out: 8x8xM  -- Check also padding here
-        for w in self.W:                
-            x = self.act(w(x))          # Out: 8x8xM  -- Check also padding here)]
-        x = x.view(x.size(0), -1)       # Out: 64*M  (M = 32 -> 2048)
-        return self.fc(x)
-
-
-
-class Conv_Recusive_Net(nn.Module):
-    
-    def __init__(self, name:str, layers:int, filters:int=32):
-        super(Conv_Recusive_Net, self).__init__()
-        
-        self.name = name
-        self.L = layers
-        self.M = filters
-        self.act = nn.ReLU()
-
-        self.V = nn.Conv2d(3,self.M,8, stride=1, padding=3)     # Out: 32x32xM  -- Maybe padding = 4?
-        self.P = nn.MaxPool2d(4, stride=4, padding=2)           # Out: 8x8xM  -- Check also padding here
-        
-        # WASAY: Shouldn't the kernel number here be M of size 3x3. Am I missing something here? 
-        self.W = nn.Conv2d(32,32,3, padding=1)                 # Out: 8x8xM  -- Check also padding here)]
-        
-        self.fc = nn.Linear(8*8*self.M, 10)
-        
-    def forward(self, x):
-        
-        x = self.act(self.V(x))
-        x = self.P(x)
-        for w in range(self.L):
-            x = self.act(self.w(x))
-        # WASAY: "The final hidden layer is subject to pixel-wise L2 normalization", do we account for that?
-        x = x.view(x.size(0), -1)
-        return self.fc(x)       # WASAY: I was wondering why we don't apply softmax here before we retun the activations.
-                                # This is clearly different from the simple convnet, where we do apply softmax.
-
-
-if '__name__' == '__main__':
-    
-    from torch.autograd import Variable
-    def test(net):
-        y = net(Variable(torch.randn(1,3,32,32)))
-        print(y.size())
-    
-    L = 16
-    M = 32
-    
-    convnet = Conv_Net('ConvNet', layers=L, filters=M)
-    r_convnet = Conv_Recusive_Net('RecursiveConvNet', layers=L, filters=M)
-    
-    test(convnet)
-    test(r_convnet)
