e71bc114606f02b39317f817a37eceefd9b552ad
PabloRR100 <pabloruizruiz10@gmail.com>
2019-02-24 13:42:46 +0100

    recursive single

--- scripts/convnet_Recursive_Ensemble.py
+++ scripts/convnet_Recursive_Ensemble.py
@@ -32,7 +32,7 @@ parser.add_argument('--filters', '-M', default=32, type=int, help='# of filters'
 parser.add_argument('--ensemble', '-es', default=5, type=int, help='ensemble size')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--comments', '-c', default=True, type=bool, help='print all the statements')
-parser.add_argument('--testing', '-t', default=False, type=bool, help='set True if running without GPU for debugging purposes')
+parser.add_argument('--test', '-t', default=False, type=bool, help='set True if running without GPU for debugging purposes')
 args = parser.parse_args()
 
 
@@ -46,8 +46,9 @@ milestones = [150, 300, 400]
 
 L = args.layers
 M = args.filters
+E = args.ensemble
 
-testing = args.testing 
+test = args.test 
 comments = args.comments
 n_workers = torch.multiprocessing.cpu_count()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -87,8 +88,8 @@ trainloader, testloader, classes = dataloaders(dataset, batch_size)
     
 avoidWarnings()
 comments = True
-from utils import count_parameters
 from models import Conv_Recusive_Net
+from utils import count_parameters
 net = Conv_Recusive_Net('recursive_net', layers=L, filters=M)
 
 
@@ -98,25 +99,36 @@ print('\n\n\t\tParameters: {}M'.format(count_parameters(net)/1e6))
 if comments: print(net)
 
 
+
+from collections import OrderedDict
+
+ensemble = OrderedDict()
+for n in range(1,1+E):
+    ensemble['net_{}'.format(n)] = Conv_Recusive_Net('net_{}'.format(n), layers=L, filters=M)
+
 if args.resume:
     
     print('==> Resuming from checkpoint..')
     print("[IMPORTANT] Don't forget to rename the results object to not overwrite!! ")
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/rec_ckpt.t7')
+    checkpoint = torch.load('./checkpoint/ckpt.t7')
     
-    net.to(device)
-    if device == 'cuda':
-        net = torch.nn.DataParallel(net)
-    net.load_state_dict(checkpoint['net'])
+    for n,net in enumerate(ensemble):
+        net.to(device)
+        if device == 'cuda':
+            net = torch.nn.DataParallel(net)
+        net.load_state_dict(checkpoint['net_{}'.format(n)])
     
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
 
 optimizers = []
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-5)
 
+for n in range(1,1+E):
+    optimizers.append(
+        optim.SGD(ensemble['net_{}'.format(n)].parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-5)
+    )
 
 
 # Training
@@ -128,8 +140,8 @@ from results import TrainResults as Results
 
 def train(epoch):
     
-    net.train()
     print('\nEpoch: %d' % epoch)
+    for net in ensemble: net.train()
 
     total = 0
     correct = 0
@@ -137,21 +149,24 @@ def train(epoch):
     
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         
-        optimizer.zero_grad()
-
-        inputs, targets = inputs.to(device), targets.to(device)
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
         
-        loss.backward()
-        optimizer.step()
+        for n, net in enumerate(ensemble):
+        
+            optimizer[n].zero_grad()
     
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            
+            loss.backward()
+            optimizer.step()
+        
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
         
         ## TODO: UNCOMMENT WHEN RUNNING ON SERVER - It just for debuggin on local
-        if testing and batch_idx == 20:
+        if test and batch_idx == 20:
             break
     
     accuracy = 100.*correct/total    
@@ -182,7 +197,7 @@ def test(epoch):
             correct += predicted.eq(targets).sum().item()
             
             # TODO: UNCOMMENT WHEN RUNNING ON SERVER -> wraped in test parameter
-            if testing and batch_idx == 20:
+            if test and batch_idx == 20:
                 break
             
     # Save checkpoint.
@@ -200,7 +215,7 @@ def test(epoch):
         }
         if not os.path.isdir('checkpoint'):
             os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/rec_ckpt.t7')
+        torch.save(state, './checkpoint/ckpt.t7')
         best_acc = acc
 
 
@@ -214,10 +229,10 @@ def lr_schedule(epoch):
     
 def results_backup():
     global results
-    with open('Results_Singe_Recursive.pkl', 'wb') as object_result:
+    with open('Results_Singe.pkl', 'wb') as object_result:
         pickle.dump(results, object_result, pickle.HIGHEST_PROTOCOL)     
 
-#@timeit
+@timeit
 def run_epoch(epoch):
     
     lr_schedule(epoch)
@@ -226,13 +241,12 @@ def run_epoch(epoch):
     results_backup()
         
     
-results = Results([net])
-net.to(device)
+results = Results([convnet])
+convnet.to(device)
 if device == 'cuda':
-    net = torch.nn.DataParallel(net)
+    convnet = torch.nn.DataParallel(convnet)
     cudnn.benchmark = True
 
-testing = True
 print('[OK]: Starting Training of Single Model')
 for epoch in range(start_epoch, num_epochs):
     run_epoch(epoch)
