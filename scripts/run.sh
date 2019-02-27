e30cf88bd14a8d974bc9494f0d5303c7bc52ac03
Abdul Wasay <awasay@g.harvard.edu>
2019-02-27 14:31:26 -0500

    local

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-timestamp() {
-  date +"%T"
-}
-
-python3 convnet.py --lr 0.1 1>./logs/temp.log 
+# 
+python3 convnet.py --lr 0.1 -n lr_0_1 -e 300
+python3 convnet.py --lr 0.01 -n lr_0_01 -e 300
+python3 convnet.py --lr 0.001 -n lr_0_001 -e 300 
