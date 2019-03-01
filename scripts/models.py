8545b8dd9e5010eeafb7cb77e89ee0b93890b2bc
PabloRR100 <pabloruizruiz10@gmail.com>
2019-03-01 10:54:03 +0100

    forcing all ReLUs to activate at the beggining

--- scripts/models.py
+++ scripts/models.py
@@ -35,11 +35,13 @@ class Conv_Net(nn.Module):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
                 if m.bias is not None:
-                    m.bias.data.zero_() ## Notes (1)
+#                    m.bias.data.zero_() ## Notes (1)
+                    m.bias.data.fill_(0.01)
                     
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
-                m.bias.data.zero_() 
+#                m.bias.data.zero_() 
+                m.bias.data.fill_(0.01)
                 
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
@@ -79,7 +81,8 @@ class Conv_Recusive_Net(nn.Module):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
                 if m.bias is not None:
-                    m.bias.data.zero_()
+#                    m.bias.data.zero_()
+                    m.bias.data.fill_(0.01)
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
