5a84a1969e12f605f4301207e644b1f3bd1480f5
PabloRR100 <pabloruizruiz10@gmail.com>
2019-03-22 16:54:59 +0100

    top1 top5

--- scripts/results.py
+++ scripts/results.py
@@ -3,6 +3,22 @@ from pandas import concat, DataFrame
 from collections import OrderedDict as ordict
 from beautifultable import BeautifulTable as BT
 
+def accuracies(output, target, topk=(1,)):
+
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
 class TrainResults(object):
     
     def __init__(self, models):
