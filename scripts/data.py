b5ce5a7057158d669a5af8d850eefc5a57183fd6
PabloRR100 <pabloruizruiz10@gmail.com>
2019-03-22 16:54:06 +0100

    simple

--- scripts/data.py
+++ scripts/data.py
@@ -25,7 +25,7 @@ def dataloaders(dataset, batch_size):
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
         
         testset = torchvision.datasets.CIFAR10(root='../../datasets', train=False, download=True, transform=transform_test)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
         
         classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
         
