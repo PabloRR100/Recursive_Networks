2a03da55f75f16d21734884c8af5de6b8630fdac
EC2 Default User <ec2-user@ip-172-31-91-129.ec2.internal>
2019-04-08 07:08:20 +0000

    some trainings0

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -5,14 +5,14 @@ source activate pytorch_p36
 
 # Big Ensemble
 
-echo Running L=6 M=64 K=4
-echo Y | python train_non_Recursive_Ensemble.py -L 6 -M 64 -K 4 | tee K_4_L_6_M_64.txt
+# echo Running L=6 M=64 K=4
+# echo Y | python train_non_Recursive_Ensemble.py -L 6 -M 64 -K 4 | tee K_4_L_6_M_64.txt
  
-echo Running L=32 M=21 K=8
-echo Y | python train_non_Recursive_Ensemble.py -L 32 -M 21 -K 8 | tee K_8_L_32_M_21.txt
+# echo Running L=32 M=21 K=8
+echo Y | python train_non_Recursive_Ensemble.py -L 32 -M 21 -K 8 --resume | tee K_8_L_32_M_21.txt
 
 echo Running L=32 M=14 K=16
-echo Y | python train_non_Recursive_Ensemble.py -L 32 -M 14 -K 16 | tee K_16_L_32_M_14.txt
+echo Y | python train_non_Recursive_Ensemble.py -L 32 -M 14 -K 16 --resume | tee K_16_L_32_M_14.txt
 
 echo Running L=2 M=64 K=8
 echo Y | python train_non_Recursive_Ensemble.py -L 2 -M 64 -K 8 | tee K_8_L_3_M_64.txt  # Good candidate to train recursive
