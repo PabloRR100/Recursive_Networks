e1c24635b2af6e96046e16221c998b5f2797584a
PabloRR100 <pabloruizruiz10@gmail.com>
2019-04-16 16:20:05 +0200

    placeholded names to run new experiments

--- scripts/train_non_Recursive_Ensemble.py
+++ scripts/train_non_Recursive_Ensemble.py
@@ -377,25 +377,43 @@ import pickle
 # Analysizing Results
 # --------------------
 
-# CANDIDATES
+# Round 2: L = 32, M = 64 --> Using scripts to build table and then chose examples
+# -----------------------
 single_prmts = {'L': 32, 'M': 64, 'BN': False} 
 #ensemble_prmts = {'L': 16, 'M': 31, 'BN': False, 'K': 4}  #  - done
 #ensemble_prmts = {'L': 4,  'M': 36, 'BN': False, 'K': 16}  # - done
 #ensemble_prmts = {'L': 4, 'M': 54, 'BN': False, 'K': 8}   #  - done 
 #ensemble_prmts = {'L': 8, 'M': 40, 'BN': False, 'K': 8}   #  - done
 
-# Round 2 Candidates
-# ------------------
+
+# Round 2: L = 32, M = 64
+# -----------------------
 
 # Horizontal Division
-ensemble_prmts = {'L': 32,  'M': 31, 'BN': False, 'K': 4}  # - done
-ensemble_prmts = {'L': 32,  'M': 21, 'BN': False, 'K': 8}  # - done
+ensemble_prmts = {'L': 32,  'M': 31, 'BN': False, 'K': 4}   # - done
+ensemble_prmts = {'L': 32,  'M': 21, 'BN': False, 'K': 8}   # - done
 ensemble_prmts = {'L': 32,  'M': 14, 'BN': False, 'K': 16}  # - done
-ensemble_prmts = {'L': 32,  'M': 21, 'BN': False, 'K': 8}  # - done
-ensemble_prmts = {'L': 32,  'M': 14, 'BN': False, 'K': 16}  # - done
-
 
 # Vertical Division
+ensemble_prmts = {'L': 2,  'M': 64, 'BN': False, 'K': 4}    # - done
+ensemble_prmts = {'L': 6,  'M': 64, 'BN': False, 'K': 8}    # - done
+
+# Round 3.1: L = 32, M = 64 -> Soft Constraint  (Me = 48)
+# --------------------------------------------------------
+# Vertical Division
+ensemble_prmts = {'L': 12,  'M': 48, 'BN': False, 'K': 4}       # - TODO
+ensemble_prmts = {'L': 5,   'M': 48, 'BN': False, 'K': 8}       # - TODO
+ensemble_prmts = {'L': 3,   'M': 48, 'BN': False, 'K': 12}      # - TODO
+ensemble_prmts = {'L': 1,   'M': 48, 'BN': False, 'K': 16}      # - TODO & RECURSIVE
+
+
+# Round 3.1: L = 32, M = 64 -> Soft Constraint  (Me = 32)
+# --------------------------------------------------------
+# Vertical Division
+ensemble_prmts = {'L': 30,  'M': 32, 'BN': False, 'K': 4}       # - TODO
+ensemble_prmts = {'L': 13,  'M': 32, 'BN': False, 'K': 8}       # - TODO
+ensemble_prmts = {'L': 8,   'M': 32, 'BN': False, 'K': 12}      # - TODO
+ensemble_prmts = {'L': 5,   'M': 32, 'BN': False, 'K': 16}      # - TODO
 
 
 path = '../results/dicts/ensemble_non_recursives/Ensemble_Non_Recursive_L_{L}_M_{M}_BN_{BN}_K_{K}.pkl'.format(**ensemble_prmts)
