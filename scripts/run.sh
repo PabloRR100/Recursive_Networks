78cdb08c4a5ed4d9b179029d3eb5d40369f8808b
PabloRR100 <pabloruizruiz10@gmail.com>
2019-04-17 09:33:35 +0200

    changes after training K_${1}_L_${2}_M_${3}

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 # Activate Environment
 # source activate pytorch_p36
+source activate pytorch
 
 # ensemble_prmts = {'L': 12,  'M': 48, 'BN': False, 'K': 4}       # - TODO
 # ensemble_prmts = {'L': 5,   'M': 48, 'BN': False, 'K': 8}       # - TODO
@@ -9,14 +10,20 @@
 # ensemble_prmts = {'L': 1,   'M': 48, 'BN': False, 'K': 16}      # - TODO & RECURSIVE
 
 
-# Big Ensemble
 E=2
 BN=False
+
+function g() {
+    git add .
+    git commit -m 'changes after training K_${1}_L_${2}_M_${3}'
+    git push
+}
+
 L=12; M=48; K=4;
-
-
 echo Running L=$L M=$M K=$K
-echo Y | python train_non_Recursive_Ensemble.py -L $L -M $M -K $K -E $KE | tee K_$K_L_$L_M_$K.txt
+echo Y | python train_non_Recursive_Ensemble.py -L $L -M $M -K $K -E $E | tee K_${K}_L_${L}_M_${M}.txt
+g "$K" "$L" "$M"
+
  
 #echo Running L=32 M=21 K=8
 #echo Y | python train_non_Recursive_Ensemble.py -L 32 -M 21 -K 8 | tee K_8_L_32_M_21_IIII.txt
