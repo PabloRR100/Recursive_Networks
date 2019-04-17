c93b533a56d5be581936acbacce755cbd18a0d64
EC2 Default User <ec2-user@ip-172-31-91-129.ec2.internal>
2019-04-17 08:05:51 +0000

    reset HEAD + preliminary results

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,46 +1,41 @@
 #!/bin/bash
 
 # Activate Environment
-# source activate pytorch_p36
-source activate pytorch
+source activate pytorch_p36
+# source activate pytorch
 
-# ensemble_prmts = {'L': 12,  'M': 48, 'BN': False, 'K': 4}       # - TODO
-# ensemble_prmts = {'L': 5,   'M': 48, 'BN': False, 'K': 8}       # - TODO
-# ensemble_prmts = {'L': 3,   'M': 48, 'BN': False, 'K': 12}      # - TODO
-# ensemble_prmts = {'L': 1,   'M': 48, 'BN': False, 'K': 16}      # - TODO & RECURSIVE
-
-
-E=2
+E=700
 BN=False
 
 function g() {
+    arg1=$1; arg2=$2; arg3=$3 
     git add .
-    git commit -m 'changes after training K_${1}_L_${2}_M_${3}'
+    git commit -m 'changes after training K_${arg1}_L_${arg2}_M_${arg3}'
     git push
 }
 
 L=12; M=48; K=4;
 echo Running L=$L M=$M K=$K
 echo Y | python train_non_Recursive_Ensemble.py -L $L -M $M -K $K -E $E | tee K_${K}_L_${L}_M_${M}.txt
-g "$K" "$L" "$M"
+# g "$K" "$L" "$M"
 
 
 L=5; M=48; K=8;
 echo Running L=$L M=$M K=$K
 echo Y | python train_non_Recursive_Ensemble.py -L $L -M $M -K $K -E $E | tee K_${K}_L_${L}_M_${M}.txt
-g "$K" "$L" "$M"
+# g "$K" "$L" "$M"
 
 
 L=3; M=48; K=12;
 echo Running L=$L M=$M K=$K
 echo Y | python train_non_Recursive_Ensemble.py -L $L -M $M -K $K -E $E | tee K_${K}_L_${L}_M_${M}.txt
-g "$K" "$L" "$M"
+# g "$K" "$L" "$M"
 
 
 L=1; M=48; K=16;
 echo Running L=$L M=$M K=$K
 echo Y | python train_non_Recursive_Ensemble.py -L $L -M $M -K $K -E $E | tee K_${K}_L_${L}_M_${M}.txt
-g "$K" "$L" "$M"
+# g "$K" "$L" "$M"
 
 
 # Recursives
@@ -48,19 +43,19 @@ g "$K" "$L" "$M"
 L=3; M=48; K=16;
 echo Running L=$L M=$M K=$K
 echo Y | python train_Recursive_Ensemble.py -L $L -M $M -K $K -E $E | tee K_${K}_L_${L}_M_${M}.txt
-g "$K" "$L" "$M"
+# g "$K" "$L" "$M"
 
 
 L=5; M=48; K=16;
 echo Running L=$L M=$M K=$K
 echo Y | python train_Recursive_Ensemble.py -L $L -M $M -K $K -E $E | tee K_${K}_L_${L}_M_${M}.txt
-g "$K" "$L" "$M"
+# g "$K" "$L" "$M"
 
 
 L=12; M=48; K=16;
 echo Running L=$L M=$M K=$K
 echo Y | python train_Recursive_Ensemble.py -L $L -M $M -K $K -E $E | tee K_${K}_L_${L}_M_${M}.txt
-g "$K" "$L" "$M"
+# g "$K" "$L" "$M"
 
 exit
 
