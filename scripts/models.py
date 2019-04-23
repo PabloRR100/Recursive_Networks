b6f69cf40e1b25ed0a727f0c31d4c4d737b4aa46
PabloRR100 <pabloruizruiz10@gmail.com>
2019-04-23 10:28:57 +0200

    model for k-recursivity

--- scripts/models.py
+++ scripts/models.py
@@ -58,10 +58,14 @@ class Conv_Net(nn.Module):
             
         if not self.normalize:
             x = self.act(self.V(x))         # Out: 32x32xM
+            print(x.shape)
             x = self.P(x)                   # Out: 8x8xM  
+            print(x.shape)
             for w in self.W:
                 x = self.act(w(x))          # Out: 8x8xM  
+                print(x.shape)
             x = x.view(x.size(0), -1)       # Out: 64*M  (M = 32 -> 2048)
+            print(x.shape)
             return self.C(x)
         
         else:
@@ -86,9 +90,7 @@ class Conv_Recusive_Net(nn.Module):
 
         self.V = nn.Conv2d(3,self.M,8, stride=1, padding=3)     # Out: 32x32xM
         self.P = nn.MaxPool2d(4, stride=4, padding=2)           # Out: 8x8xM  
-        
         self.W = nn.Conv2d(self.M,self.M,3, padding=1)          # Out: 8x8xM 
-        
         self.C = nn.Linear(8*8*self.M, 10)
         
         # Custom Initialization
@@ -97,7 +99,6 @@ class Conv_Recusive_Net(nn.Module):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
                 if m.bias is not None:
-#                    m.bias.data.zero_()
                     m.bias.data.fill_(0.01)
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
@@ -113,70 +114,27 @@ class Conv_Recusive_Net(nn.Module):
         x = x.view(x.size(0), -1)
         return self.C(x)  
    
-    
-    
-class Conv_Custom_Recusive_Net(nn.Module):
-    
-    def __init__(self, name:str, L:int, M:int=32, F:int=32):
-        super(Conv_Custom_Recusive_Net, self).__init__()
-        
-        self.L = L
-        self.M = M
-        self.F = F
-        self.name = name
-        self.act = nn.ReLU(inplace=True)
-
-        self.V = nn.Conv2d(3,self.M,8, stride=1, padding=3)     # Out: 32x32xM
-        self.P = nn.MaxPool2d(4, stride=4, padding=2)           # Out: 8x8xM  
-        
-        self.W = nn.Conv2d(self.M,self.M,3, padding=1)          # Out: 8x8xM 
-        self.WL = nn.Conv2d(self.F,self.F,3,padding=1)          # Out: 8x8xF
-        
-        self.C = nn.Linear(8*8*self.F, 10)
-        
-        # Custom Initialization
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-#                    m.bias.data.zero_()
-                    m.bias.data.fill_(0.01)
-            elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
-                        
-    def forward(self, x):
-        
-        x = self.act(self.V(x))
-        x = self.P(x)
-        for w in range(self.L - 1):     # Up to layer L not included
-            x = self.act(self.W(x))
-        x = self.act(self.WL(x))        # Last convolution use WL with F filters
-        x = x.view(x.size(0), -1)
-        return self.C(x)  
-    
 
 
 class Conv_K_Recusive_Net(nn.Module):
     '''
     Recursive block of K layers
     '''
-    def __init__(self, name:str, L:int, M:int=32, K:int=2):
+    def __init__(self, name:str, Lo:int, Lr:int, M:int=32):
         super(Conv_K_Recusive_Net, self).__init__()
         
-        self.K = K
-        self.L = L
-        self.M = M
         self.name = name
+
+        self.M = M
+        self.R = math.ceil(Lr/Lo) # Recursivity withing each block
         self.act = nn.ReLU(inplace=True)
+        self.B = [self.R] * (Lo-1) + [Lr%self.R] if Lr%self.R != 0 else [self.R] * Lo
 
         self.V = nn.Conv2d(3,self.M,8, stride=1, padding=3)     # Out: 32x32xM
         self.P = nn.MaxPool2d(4, stride=4, padding=2)           # Out: 8x8xM 
         
-#        self.W = nn.Conv2d(self.M,self.M,3, padding=1)          # Out: 8x8xM  
         self.Wk = nn.ModuleList(                                 
-            [nn.Conv2d(self.M,self.M,3,1) for _ in range(self.K)])
+            [nn.Conv2d(self.M,self.M,3, padding=1) for _ in range(self.R)])  # Out: 8x8xM  
         
         self.C = nn.Linear(8*8*self.M, 10)
         
@@ -186,7 +144,6 @@ class Conv_K_Recusive_Net(nn.Module):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
                 if m.bias is not None:
-#                    m.bias.data.zero_()
                     m.bias.data.fill_(0.01)
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
@@ -198,10 +155,10 @@ class Conv_K_Recusive_Net(nn.Module):
         x = self.act(self.V(x))
         x = self.P(x)
         
-        for block in range(int(self.L/self.K)):     # num_blocks = num_layers / layers_per_block
-            for W in self.Wk:                       # for each layer in the block
+        for b,W in zip(self.B, self.Wk):    # for each layer
+            for _ in range(b):              # run a recursive block
                 x = self.act(W(x))
-
+    
         x = x.view(x.size(0), -1)
         return self.C(x) 
 
@@ -218,15 +175,18 @@ if '__name__' == '__main__':
     K = 2
     F = 16
     
+    M = 4
+    Lo = 5
+    Lr = 30
+
+    
     convnet = Conv_Net('ConvNet', L, M)
     r_convnet = Conv_Recusive_Net('RecursiveConvNet', L, M)
-    r_convnet_k = Conv_K_Recusive_Net('Custom_Recursive_ConvNet', L, M, K)
-    r_convnet_c = Conv_Custom_Recusive_Net('Custom_Recursive_ConvNet', L, M, F)
+    r_convnet_k = Conv_K_Recusive_Net('Custom_Recursive_ConvNet', Lo, Lr, M)
 
     test(convnet)
     test(r_convnet)
     test(r_convnet_k)
-    test(r_convnet_c)    
     
     exit()
     
@@ -280,58 +240,3 @@ if '__name__' == '__main__':
     ensembles = [ensemble1, ensemble2]
     
     images, labels = next(iter(trainloader))
-    
-    
-    
-
-
-
-
-
-
-    
-## Fully Connected Networks
-#
-#class FC_Net(nn.Module):
-#    ''' Fully Connected Network '''
-#    
-#    def __init__(self, name:str, inp:int, out:int, hid:int):
-#        super(FC_Net, self).__init__()
-#                
-#        self.name = name
-#        self.lay_size = hid
-#        self.act = nn.ReLU()
-#        
-#        self.fcI = nn.Linear(inp, hid, bias=True)        
-#        self.fcH = nn.Linear(hid, hid, bias=True)   
-#        self.fcO = nn.Linear(hid, out, bias=True)
-#                
-#    def forward(self, x):
-#                
-#        x = self.act(self.fcI(x))        
-#        x = self.act(self.fcH(x))            
-#        return self.fcO(x)
-#    
-#
-#class FC_Recursive_Net(nn.Module):
-#    ''' Fully Connected Network with Recursivity '''
-#    
-#    def __init__(self, name:str, inp:int, out:int, hid:int, rec:int):
-#        super(FC_Recursive_Net, self).__init__()
-#    
-#        self.name = name
-#        self.n_lay = rec        
-#        self.act = nn.ReLU()
-#        assert rec > 0, 'Recursive parameters must be >= 1'
-#        
-#        self.fcI = nn.Linear(inp, hid, bias=True)        
-#        self.fcH = nn.Linear(hid, hid, bias=True) 
-#        self.fcO = nn.Linear(hid, out, bias=True)
-#                
-#    def forward(self, x):
-#                
-#        x = self.act(self.fcI(x))        
-#        x = self.act(self.fcH(x))
-#        for l in range(self.n_lay): 
-#            x = self.actf(self.fcHid(x))            
-#        return self.fcO(x)
